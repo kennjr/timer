62c081b4b5218a44d19c9fc8423ade9581097aa7
kenedy <kenedy@myhechar.pro>
2022-11-03 23:29:08 +0300

    added the timer to the screen

--- app/src/main/java/com/ramanie/timer/MainActivity.kt
+++ app/src/main/java/com/ramanie/timer/MainActivity.kt
@@ -3,21 +3,26 @@ package com.ramanie.timer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ramanie.timer.ui.theme.TimerTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimerTheme {
-
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Box(contentAlignment = Alignment.Center){
+                    Timer(totalTime = 60000, handleColor = Color.Green,
+                        inactiveBarColor = Color.DarkGray,
+                        activeBarColor = Color(0xFF37B900),
+                        modifier = Modifier.size(200.dp))
+                }
             }
         }
     }
